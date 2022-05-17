c06e9cb93f417a0cd7ee6fe36420084309c2f0b3
chrisrandall <chris.f.randall@gmail.com>
2022-05-17 15:57:20 -0600

    commit tree 826340dcba8363f312118402fb3afab71995d434

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+superbowlWin = (record) => {
+    const result = record.find(winFinder);
+    // if(result === undefined){
+    //     return undefined;
+    // }
+    // return result.year;
+    return result ? result.year : undefined;
+}
+
+function winFinder(object){
+    if(object.result === "W"){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
